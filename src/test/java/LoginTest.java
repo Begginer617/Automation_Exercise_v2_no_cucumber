cb782c0a4f6346bb247e6bf81a18fe076a0bb841
Begginer617 <75578722+Begginer617@users.noreply.github.com>
2024-10-27 19:12:01 +0100

    firsts loggin tests

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -5,15 +5,20 @@ import org.openqa.selenium.WebElement;
 public class LoginTest extends BaseTest {
 
     @Test
-    public void successfulLogging(){
+    public void successfulLogging() {
+        // Navigate to the login page
         driver.get("https://automationexercise.com/login");
-        WebElement usernameField = driver.findElement(By.name("username"));
-        WebElement passwordField = driver.findElement(By.name("password"));
+
+        // Find the username and password input fields
+        WebElement usernameField = driver.findElement(By.name("email")); // Use the correct name attribute for the email field
+        WebElement passwordField = driver.findElement(By.name("password")); // Use the correct name attribute for the password field
+
+        // Input the username and password
+        usernameField.sendKeys("sisopay609@digopm.com");
+        passwordField.sendKeys("sisopay609@digopm.com");
+
+        // Submit the form
+        passwordField.submit();
+
     }
-
-
-
-
-
-
 }
